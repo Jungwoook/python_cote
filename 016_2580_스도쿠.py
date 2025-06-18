1cea739737bf04e1be6fbc1c68e8c3caf78191ec
Jungwoook <okjungwoo@gmail.com>
2025-06-18 09:51:02 +0900

    016 backtrack add

--- 016_2580_스도쿠.py
+++ 016_2580_스도쿠.py
@@ -10,15 +10,15 @@ sqs = [set() for _ in range(9)]
 for i, line in enumerate(board):
     for j, val in enumerate(line):
         if val != 0:
-            rows[j].add(val)
-            cols[i].add(val)
+            rows[i].add(val)
+            cols[j].add(val)
             sqs[(i // 3 * 3) + j // 3].add(val)
 
 def isValid(x, y, i):
-    if i in rows[y]:
+    if i in rows[x]:
         return False
     
-    if i in cols[x]:
+    if i in cols[y]:
         return False
     
     if i in sqs[(x // 3 * 3) + y // 3]:
@@ -33,13 +33,24 @@ def dfs(index):
             for i in li:
                 print(i, end=' ')
             print()
-        return
+        sys.exit(0)
 
     for i in range(1, 10):
         # blank를 순서대로 넣고 dfs
-        board[blanks[index][0]][blanks[index][1]] = i
         # isValid 체크
         if isValid(blanks[index][0], blanks[index][1], i): # 유효하면
+
+            board[blanks[index][0]][blanks[index][1]] = i
+            rows[blanks[index][0]].add(i)
+            cols[blanks[index][1]].add(i)
+            sqs[(blanks[index][0] // 3 * 3) + blanks[index][1] // 3].add(i)
+
             dfs(index + 1)
+            
+            # 되돌리기
+            board[blanks[index][0]][blanks[index][1]] = 0
+            rows[blanks[index][0]].remove(i)
+            cols[blanks[index][1]].remove(i)
+            sqs[(blanks[index][0] // 3 * 3) + blanks[index][1] // 3].remove(i)
         
 dfs(0)
